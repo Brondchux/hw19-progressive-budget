5c8d1efc119520ae1ab938f86d5f43391d85390d
Gospel Chukwu <brondchux@gmail.com>
2021-09-15 08:38:04 -0400

    Successfully cached static data files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,37 @@
 console.log("Service worker registered and ready for action");
+const CACHE_NAME = "budget-static-files-v1";
+const DATA_CACHE_NAME = "budget-data-files-v1";
+
+const STATIC_FILES_TO_CACHE = [
+	"/",
+	"/index.html",
+	"/manifest.json",
+	"/service-worker.js",
+	"/assets/js/index.js",
+	"/assets/css/styles.css",
+	"/assets/icons/icon-192x192.png",
+	"/assets/icons/icon-512x512.png",
+];
+
+const DATA_FILES_TO_CACHE = [
+	"/api/transaction",
+	"/api/transaction/bulk",
+	"/api/transaction",
+];
+
+// add files to cache
+self.addEventListener("install", function (event) {
+	// add static files
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			return cache.addAll(STATIC_FILES_TO_CACHE);
+		})
+	);
+
+	// add data files
+	event.waitUntil(
+		caches.open(DATA_CACHE_NAME).then(function (cache) {
+			return cache.addAll(DATA_FILES_TO_CACHE);
+		})
+	);
+});
