51c77ff98f5f01151cdaa8a77e8bd80143c55918
Gospel Chukwu <brondchux@gmail.com>
2021-09-15 09:01:17 -0400

    Successfully removed old cached files both static and dynamic

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budget-static-files-v1";
+const STATIC_CACHE_NAME = "budget-static-files-v1";
 const DATA_CACHE_NAME = "budget-data-files-v1";
 
 const STATIC_FILES_TO_CACHE = [
@@ -12,11 +12,11 @@ const STATIC_FILES_TO_CACHE = [
 	"/assets/icons/icon-512x512.png",
 ];
 
-// add files to cache
+// Add new files to cache
 self.addEventListener("install", function (event) {
 	// add static files
 	event.waitUntil(
-		caches.open(CACHE_NAME).then(function (cache) {
+		caches.open(STATIC_CACHE_NAME).then(function (cache) {
 			return cache.addAll(STATIC_FILES_TO_CACHE);
 		})
 	);
@@ -25,6 +25,24 @@ self.addEventListener("install", function (event) {
 	event.waitUntil(
 		caches.open(DATA_CACHE_NAME).then(function (cache) {
 			cache.add("/api/transaction");
+		})
+	);
+});
+
+// Remove old cache
+self.addEventListener("activate", function (event) {
+	event.waitUntil(
+		caches.keys().then(function (cacheNames) {
+			return Promise.all(
+				cacheNames.map(function (cacheName) {
+					if (
+						cacheName !== STATIC_CACHE_NAME &&
+						cacheName !== DATA_CACHE_NAME
+					) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
 		})
 	);
 });
